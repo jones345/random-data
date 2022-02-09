bbc5c10832e1ee03c15e3b46a7c2918c71066bcf
zntongana <zntongana@bitri.co.bw>
2022-02-09 15:36:01 +0200

    data more data

--- cont.js
+++ cont.js
@@ -4,66 +4,71 @@ const fs = require('fs');
 
 
 var subscriber = []
-
-
+//1000000
+const million = '1000000'
 const start = ()=>{
+
+        
+  for(var i = 0; i < 10; i++){
+
+    var nationalityData= null
+    var documentNumberData = null;
+  
+   const  firstNameData = faker.name.firstName()
+   const lastNameData = faker.name.lastName()
+   const genderData =faker.random.arrayElement(["FEMALE", "MALE"])
    
-
-    for(var i = 0; i < 1; i++){
-
-        var nationalityData= null
-        const dateOfbirthdata = faker.date.between('2009-01-01', '2020-01-01')
-       const  firstNameData = faker.name.firstName()
-       const genderData =faker.random.arrayElement(["FEMALE", "MALE"])
-        const lastNameData = faker.name.lastName()
-       const msisdnData =`${faker.datatype.number({ min: 71000000, max: 71099999 })}`
-       const  numberStatusData =faker.random.arrayElement(["INACTIVE", "ACTIVE","TEMPORARILY_BARRED","SUSPENDED"])
-        const cityTownData = faker.address.city()
-        const plotNumberData = faker.address.secondaryAddress()
-        const wardDataData = faker.address.streetName()
-        const boxNumberData = faker.address.secondaryAddress()
-        const documentTypeData = faker.random.arrayElement(["PASSPORT", "NATIONAL_ID"])
-        const documentNumberData = `${faker.datatype.number({ min: 10000000000, max: 900000000000 })}`
-        const dateOfIssueData = faker.date.between('2009-01-01', '2020-01-01')
-        const expiryDateData = faker.date.between('2021-01-01', '2030-01-01')
-        //check value of  documentTypeData
-        if(documentTypeData === "PASSPORT"){
-            nationalityData = faker.address.country()
-        }
-        else{
-            nationalityData = "Botswana"
-        }
-
-
-        subscriber.push({
-            dateOfbirth:dateOfbirthdata,
-            documents: [{
-                dateOfIssue: dateOfIssueData,
-                expiryDate: expiryDateData,
-                documentType: documentTypeData,
-                documentNumber: documentNumberData
-            }],
-            firstName: firstNameData,
-            gender:genderData,
-            lastName:lastNameData,
-            nationality:nationalityData,
-            msisdn:msisdnData,
-            numberStatus:numberStatusData,
-            physicalAddress:{
-                cityTown:cityTownData,
-                plotNumber:plotNumberData,
-                ward:wardDataData,
-            },
-            postalAddress:{
-                boxNumber:boxNumberData,
-                cityTown:cityTownData  
-            },
-        })
-        // console.log(subscriber)
-        console.log(JSON.stringify(subscriber, null, 2))
+   const msisdnData =`${faker.datatype.number({ min: 71000000, max: 71099999 })}`
+   const  numberStatusData =faker.random.arrayElement(["INACTIVE", "ACTIVE","TEMPORARILY_BARRED","SUSPENDED"])
+    const cityTownData = faker.address.city()
+    const dateOfbirthdata = faker.date.between('2009-01-01', '2020-01-01')
+    const plotNumberData = faker.address.secondaryAddress()
+    const wardDataData = faker.address.streetName()
+    const boxNumberData = faker.address.secondaryAddress()
+    const documentTypeData = faker.random.arrayElement(["PASSPORT", "NATIONAL_ID"])
+    const dateOfIssueData = faker.date.between('2009-01-01', '2020-01-01')
+    const expiryDateData = faker.date.between('2021-01-01', '2030-01-01')
+    //check value of  documentTypeData
+    if(documentTypeData === "PASSPORT"){
+        nationalityData = faker.address.country()
+        documentNumberData = `${faker.datatype.number({ min: 10000000000, max: 900000000000 })}`
+    }
+    else{
+        nationalityData = "Botswana"
+        documentNumberData = `${faker.datatype.number({ min: 100000000, max: 900000000 })}`
     }
 
-}
+    subscriber.push({
+        firstName: firstNameData,
+        lastName:lastNameData,
+        nationality:nationalityData,
+        dateOfBirth:dateOfbirthdata,
+        sex:genderData,
+        msisdn:msisdnData,
+        mnoReference:"MNOref",
+        
+        documents: [{
+            documentNumber: documentNumberData,
+            documentType: documentTypeData,
+            dateOfIssue: dateOfIssueData,
+            expiryDate: expiryDateData
+        }],
+    
+        addresses:[{
+            plotWardBox:plotNumberData,
+            cityTown:cityTownData,
+            addressType:"PHYSICAL",
+        }]
+    })
 
-//call start function
+  }
+
+
+        setTimeout(() => {
+             console.log(subscriber.length)
+            fs.writeFileSync(`subscriber_data10.json`, JSON.stringify(subscriber), 'utf8');
+        }, 5000);
+    }
+
+
 start()
